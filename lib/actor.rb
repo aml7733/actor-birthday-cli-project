0a3c46464cf5a5aba40fa6d095bb1f230226d525
Adam Lucia <lucia.adam@gmail.com>
2016-12-19 05:09:39 +0000

    actor methods added

--- lib/actor.rb
+++ lib/actor.rb
@@ -1,13 +1,35 @@
 require_relative "../config/environment.rb"
 
 class Actor
-  # Want attr_accessor for :name, :location, :year, :profile_url, :occupations, :bio_intro, :known_for, :trivia_fact
+  attr_accessor :name, :location, :profile_url, :occupations, :bio_intro, :known_for, :trivia_fact
 
-  # Has @@all, class method for retrieval
+  @@all = []
 
-  # initialize takes attribute hash (could use send and iterator, but hash will always be the same...) and initializes actor with named variables.  pushes new actor to @@all
+  def initialize(attribute_hash)
+    @name = attribute_hash[:name]
+    @location = attribute_hash[:location]
+    @profile_url = attribute_hash[:profile_url]
 
-  # self.create_from_hashes(actorhash_array) takes in array of hashes with :name, :year, :location, :profile_url and creates an actor for each element with the appropriate values saved.
+    @@all << self
+  end
 
-  # add_attributes(attributes_hash) instance method - takes a hash with attribute key-value pairs and adds those attributes to the instance of actor called upon
+  def self.create_from_array(actor_hash_array)
+    actor_hash_array.each { |actor_hash| self.new(actor_hash) }
+  end
+
+  def add_attributes(attributes_hash)
+    attributes_hash.each do |attr_name, value|
+      self.attr_name = value
+    end
+  end
 end
+
+# Want attr_accessor for :name, :location, :year, :profile_url, :occupations, :bio_intro, :known_for, :trivia_fact
+
+# Has @@all, class method for retrieval
+
+# initialize takes attribute hash (could use send and iterator, but hash will always be the same...) and initializes actor with named variables.  pushes new actor to @@all
+
+# self.create_from_hashes(actorhash_array) takes in array of hashes with :name, :year, :location, :profile_url and creates an actor for each element with the appropriate values saved.
+
+# add_attributes(attributes_hash) instance method - takes a hash with attribute key-value pairs and adds those attributes to the instance of actor called upon
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,5 +4,4 @@ class Scraper
   # self.scrape_date_page => should use open uri to target site ("https://www.imdb.com/date" - doesn't need argument, same site every time), use nokogiri to separate out actors into array of xml actor elements, then set up a hash for each actor with :name, :year, :location, and :profile_url keys.  Should return array of actor hashes
 
   #self.scrape_profile_page(profile_url) => use open uri to open profile page, use nokogiri return hash of attributes key-value pairs (bio intro paragraph, etc.)
-  puts "This is the scraper."
 end
